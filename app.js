277c528ac56f20ed50cdaf83f3883229937482c8
Jack Reedgit config --global user.name Jack <phillipjreed@gmail.com>
2013-03-15 21:55:22 -0400

    changed app port

--- app.js
+++ app.js
@@ -41,7 +41,8 @@ app.get('/api/name', api.name);
 app.get('*', routes.index);
 
 // Start server
+var port = process.env.PORT || 3000;
 
-app.listen(3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+app.listen(port, function() {
+	console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
