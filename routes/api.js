5c05b949507b92defc3b44b0e86310f905ff5535
Jack Reedgit config --global user.name Jack <phillipjreed@gmail.com>
2013-03-18 17:57:53 -0400

    sms now working

--- routes/api.js
+++ routes/api.js
@@ -1,19 +1,20 @@
-
 //4048095070
 
-ACCOUNT_SID = 'ACe933fd0423b4672220d2a1bd8f99dd1d';
-AUTH_TOKEN = '35966416884bca65cfc25f1a13c62e40';
+var ACCOUNT_SID = process.env.TWILIO_SID;
+var AUTH_TOKEN = process.env.TWILIO_TOKEN;
+var twilio_from_number = process.env.TWILIO_FROM;
 
 var request = require('request');
 var mongoose = require("mongoose");
 var Twit = require('twit');
 var client = require('twilio')('ACCOUNT_SID', 'AUTH_TOKEN');
+var util = require('util')
 
 var T = new Twit({
-	consumer_key : 'BSs5VlwsHz4sDN45vzfiuQ',
-	consumer_secret : 'D36illj5Khhmgq7j8GSewEamLrpf5zZhBv33Ha0mPMg',
-	access_token : '1272293040-tOkdQZ3j3rXfm4Y7v2leHAmMHjuj1rutwGE4mMY',
-	access_token_secret : 'krBXYTg42vx0LJWGkppgggaEyeD1A3sGuzSVZcZFk'
+	consumer_key : process.env.TWITTER_CONSUMER_KEY,
+	consumer_secret : process.env.TWITTER_CONSUMER_SECRET,
+	access_token : process.env.TWITTER_ACCESS_TOKEN,
+	access_token_secret : process.env.TWITTER_ACCESS_TOKEN_SECRET
 })
 
 var uristring = process.env.MONGODB_URI || process.env.MONGOLAB_URI || 'mongodb://localhost/FinalFourVolunteer';
@@ -105,7 +106,10 @@ exports.alertpost = function(req, res) {
 		//  ...
 		console.log('Twitter post fail: ' + err)
 	})
-
+	sendSMSMessage(req.body.alertText)
+	//sss();
+}
+function sss() {
 	client.sendSms({
 		to : '+14043763550', // Any number Twilio can deliver to
 		from : '+16786819857', // A number you bought from Twilio and can use for outbound communication
@@ -127,6 +131,37 @@ exports.alertpost = function(req, res) {
 		}
 	});
 }
+
+function sendSMSMessage(message) {
+	var twilio_to_number = "+14043763550"
+	//return process.nextTick(function() {
+	var https, options, post_data, request;
+	https = require('https');
+	post_data = "From=" + twilio_from_number + "&To=" + twilio_to_number + "&Body=" + message;
+	options = {
+		host : 'api.twilio.com',
+		path : "/2010-04-01/Accounts/" + ACCOUNT_SID + "/SMS/Messages.json",
+		method : 'POST',
+		headers : {
+			'Content-Type' : 'application/x-www-form-urlencoded',
+			'Content-Length' : post_data.length,
+			'Authorization' : 'Basic ' + new Buffer(ACCOUNT_SID + ':' + AUTH_TOKEN).toString('base64')
+		}
+	};
+	request = https.request(options, function(response) {
+		var str;
+		str = '';
+		response.on('data', function(chunk) {
+			return str += chunk;
+		});
+		return response.on('end', function() {
+			return console.log(util.inspect(str));
+		});
+	});
+	request.write(post_data);
+	return request.end;
+	//});
+};
 
 exports.signuppost = function(req, res) {
 	console.log(req)
