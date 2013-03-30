23d790048910a321cd265a071c4262ecb84a563f
Jack Reedgit config --global user.name Jack <phillipjreed@gmail.com>
2013-03-30 15:37:52 -0400

    more logging

--- routes/index.js
+++ routes/index.js
@@ -4,28 +4,30 @@
 var userName = ""
 
 exports.index = function(req, res) {
+	console.log(req.user)
 	res.render('index', {
 		user : req.user
-
 	});
 };
-
 
 exports.about = function(req, res) {
+	console.log(req.user)
 	res.render('about', {
 		user : req.user
-
 	});
 };
 
-
 exports.shuttle = function(req, res) {
+	console.log(req.user)
+
 	res.render('shuttle', {
 		user : req.user
 	});
 }
 
 exports.register = function(req, res) {
+	console.log(req.user)
+
 	res.render('register');
 }
 
